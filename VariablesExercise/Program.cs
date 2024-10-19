fea083a24080a963e63d62ebdf74dc1a4a828a50
Your NameJoshuagit config --global user.name Joshua[200~git config --global user.email email@example.comgit config --global user.name Your <email@example.comjzadnik17@gmail.com>
2024-10-18 22:55:47 -0400

    initial commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -2,9 +2,20 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+     
+
+        static void Main(string[] ags)
         {
-            Console.WriteLine("Hello, World!");
+            string myName = "Joshua";
+           int age = 32;
+           double avg = 98.7;
+           decimal bankAccount = 10000.00m;
+            char initial = 'J';
+           bool isMarried = true;
+
+            Console.WriteLine($"Name: {myName}\nAge: {age}\nAverage {avg}\nMoney: {bankAccount}\nInitial: {initial}\nMarried: {isMarried}");
+
+   
         }
     }
 }
